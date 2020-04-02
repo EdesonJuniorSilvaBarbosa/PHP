e59e9378de9dc72ace2fb2e59b0a2c06cda110eb
Edeson Junior <edeson.jsbarbosa@hotmail.com>
2020-04-02 15:46:27 -0300

    resolvi o conflito 2

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 ﻿<?php
 
-<<<<<<< HEAD
     echo "Olá Mundo 456!!!";
 
-   echo "Udemy";
-
-=======
-    echo "Olá Mundo 123!!!";
-    
-    echo "Olá Hcode !!!";
->>>>>>> 08f6273131b09cad40cfc2eb0180f37d85680127
+    echo "Udemy";
 
 ?>
